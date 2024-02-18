3f023841ecd00261b7ea1a5c51c0f7c53c7bce2f
AbdulAhadSiddiqui11 <abdulahadsiddiqui011@gmail.com>
2024-02-19 01:53:39 +0530

    Details card

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import Button from "@mui/material/Button";
 import HeaderBar from "./components/HeaderBar";
+import DetailsCard from "./components/DetailsCard";
+
+import data from "../resources/data/data.json";
 
 const App = () => {
   return (
     <div className="app">
       <HeaderBar />
+      <div style={{display: 'flex', flexWrap: 'wrap'}}>
+        <DetailsCard data={data} />
+        <DetailsCard data={data}/>
+      </div>
+      
     </div>
   );
 };
---
+++ src/components/DetailsCard.jsx
@@ -0,0 +1,103 @@
+import React from "react";
+import { Card, CardContent, Typography, Link, Chip } from "@mui/material";
+
+import clipIcon from "../../resources/icons/attachment.svg";
+
+const DetailsCard = ({ data }) => {
+  const { article } = data;
+
+  const renderFeatures = () => {
+    return (
+      <ul>
+        {Object.entries(article.features).map(([feature, description]) => (
+          <li key={feature}>
+            <Typography variant="body1" color="textSecondary" component="span">
+              {feature}:
+            </Typography>{" "}
+            <Typography variant="body1" color="textPrimary" component="span">
+              {description}
+            </Typography>
+          </li>
+        ))}
+      </ul>
+    );
+  };
+
+  const renderKeywords = () => {
+    return article.keywords.map((keyword, index) => (
+      <Chip
+        key={index}
+        label={keyword}
+        style={{
+          marginRight: "5px",
+          marginBottom: "5px",
+          backgroundColor: "black",
+          color: "white",
+        }}
+      />
+    ));
+  };
+
+  const renderAttachments = () => {
+    return article.attachments.map((attachment, index) => (
+      <div
+        key={index}
+        style={{ display: "flex", alignItems: "center", marginTop: "5px" }}
+      >
+        <img src={clipIcon} alt="attachment" style={{ width: "15px" }} />
+        <Link
+          href={attachment.file_link}
+          style={{ marginLeft: "5px", textDecoration: "none" }}
+        >
+          {attachment.file_label}
+        </Link>
+      </div>
+    ));
+  };
+
+  return (
+    <Card variant="outlined">
+      <CardContent>
+        <Typography
+          component="div"
+          style={{
+            color: "#e14e42",
+            borderBottom: "1px solid #ccc",
+            paddingBottom: "5px",
+            marginBottom: "15px",
+          }}
+        >
+          DETAILS
+        </Typography>
+
+        <Typography
+          component="div"
+          color="textSecondary"
+        >
+          Features
+        </Typography>
+        {renderFeatures()}
+
+        <Typography
+          component="div"
+          color="textSecondary"
+          style={{ margin: "5px" }}
+        >
+          Attachments
+        </Typography>
+        <div style={{ marginBottom: "10px" }}>{renderAttachments()}</div>
+
+        <Typography
+          component="div"
+          color="textSecondary"
+          style={{ margin: "5px" }}
+        >
+          Keyboards
+        </Typography>
+        {renderKeywords()}
+      </CardContent>
+    </Card>
+  );
+};
+
+export default DetailsCard;
